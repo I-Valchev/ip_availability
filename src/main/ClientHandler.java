f3a12bd7b204451cf0069b221cc11342be494a11
I-Valchev <Ivo.V.Valchev@gmail.com>
2015-11-12 14:10:11 +0200

    Fixing shutdown issue with outdated constant of the logout command

--- src/main/ClientHandler.java
+++ src/main/ClientHandler.java
@@ -5,46 +5,46 @@ import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ClientHandler implements Runnable{
+public class ClientHandler implements Runnable {
 	private final Socket socket;
 	protected User user;
-	
-	private static final String LOGOUT_COMMAND = "%s:logout";
+
+	private static final String LOGOUT_COMMAND = "logout";
 
 	public ClientHandler(Socket socket) {
 		this.socket = socket;
 		user = new User();
 	}
-	
+
 	@Override
-	public void run(){
+	public void run() {
 		try {
 			PrintStream out = new PrintStream(socket.getOutputStream());
-		
+
 			final Scanner scanner = new Scanner(socket.getInputStream());
-		
-			while(scanner.hasNextLine()){
+
+			while (scanner.hasNextLine()) {
 				final String line = scanner.nextLine();
 				CommandHandler handler = Server.parse(line, this);
 				out.println(handler.execute());
-			}	
+			}
+
 			scanner.close();
 			out.close();
-			
-		}catch (IOException e) {
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				socket.close();
+			} catch (IOException e) {
 				e.printStackTrace();
-		}finally{
-			if(socket != null)
-				try {
-					socket.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			Server.parse(String.format(LOGOUT_COMMAND, user.name), this).execute();
+			}
+			Server.parse(String.format(LOGOUT_COMMAND), this).execute();
 		}
 	}
-	
-	public void stopClient() throws IOException{
+
+	public void stopClient() throws IOException {
 		socket.close();
 	}
 }
