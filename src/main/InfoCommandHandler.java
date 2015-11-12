3d98ed8ca99177ceb1918337f659ec79b9c08126
I-Valchev <Ivo.V.Valchev@gmail.com>
2015-11-12 09:20:37 +0200

    Reducing IFs in InfoCommandHandler.perform by overriding authenticate

--- src/main/InfoCommandHandler.java
+++ src/main/InfoCommandHandler.java
@@ -12,26 +12,28 @@ public class InfoCommandHandler extends CommandHandler {
 	}
 
 	@Override
+	public boolean authenticate() {
+		return (students.containsKey(executorName) && students.get(executorName).isIn())
+				&& (students.containsKey(targetName));
+	}
+
+	@Override
 	protected String perform() {
 		StringBuilder str = new StringBuilder();
-		if (students.containsKey(targetName)) {
-			Student student = students.get(targetName);
-			int loginCount = student.records.size();
-			str.append("ok:" + targetName + ":" + student.isIn()+":" + loginCount + ":");
-			for (Record r : student.records) {
-				Date inDate = r.getInTime();
-				Date outDate = r.getOutTime();
-				DateFormat timeFormatter = new SimpleDateFormat("yyyy-­MM-­dd'T'HH'_'mm'_'ss.SSSZ");
-				str.append(timeFormatter.format(inDate));
-				if (outDate != null)
-					str.append(timeFormatter.format(outDate));
-			}
-		}else{
-			str.append("error:notlogged");
-		}
+		Student student = students.get(targetName);
+		int loginCount = student.records.size();
+		str.append("ok:" + targetName + ":" + student.isIn() + ":" + loginCount + ":");
 		
-		return str.toString();
+		for (Record r : student.records) {
+			Date inDate = r.getInTime();
+			Date outDate = r.getOutTime();
+			DateFormat timeFormatter = new SimpleDateFormat("yyyy-­MM-­dd'T'HH'_'mm'_'ss.SSSZ");
+			str.append(timeFormatter.format(inDate));
+			if (outDate != null)
+				str.append(timeFormatter.format(outDate));
+		}
 
+		return str.toString();
 	}
 
 }
