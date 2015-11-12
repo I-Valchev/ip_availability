c2379dea567e85e5d059d97a8cc2a27c8ecc5c4e
I-Valchev <Ivo.V.Valchev@gmail.com>
2015-11-12 10:16:07 +0200

    Throwing exception if another user executes command from current socket

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,12 @@
 package main;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 	public static Server server;
 
 	public static void main(String[] args) throws IOException {	
-		server = new Server(9006);
+		server = new Server(9010);
 		server.StartServer();
 	}
 }
--- src/main/ShutdownCommandHandler.java
+++ src/main/ShutdownCommandHandler.java
@@ -1,7 +1,5 @@
 package main;
 
-import java.util.Map;
-
 public class ShutdownCommandHandler extends CommandHandler {
 	
 	Server server;
